cecc34b36a78d07157b3cca268f3fc2747520ecf
Igor <igormgg@hotmail.com>
2021-10-05 12:15:14 -0300

    feat: logic done

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import ProductList from "./components/Product-list";
+import { CartProvider } from "./providers/cart";
+import { CatalogueProvider } from "./providers/catalogue";
+
+function App() {
+  return (
+    <div className="App">
+      <CatalogueProvider>
+        <CartProvider>
+          <ProductList type="catalogue" />
+          <ProductList type="cart" />
+        </CartProvider>
+      </CatalogueProvider>
+    </div>
+  );
+}
+
+export default App;
